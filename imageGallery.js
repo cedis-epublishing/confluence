71456aba54db14f7bc67adf1444124341a19494e
lilients <svantje.lilienthal@langsci-press.org>
2015-07-24 18:01:56 +0200

    german locale added

--- imageGallery.js
+++ imageGallery.js
@@ -4,19 +4,22 @@ $(document).ready(function() {
 	var newTitle = $("#title-text").html().split(" : ");
 	$("#title-text").html(newTitle[1]);
 				
-	// wrap img in link 
+	// wrap each img in link 
 	$(".gallery-image").each(function(indexImage){
-				
+		
+		// change url of the image
 		var src = $(this).find("img").attr("src").split("/").pop();
 		src = "http://localhost/ojs/index.php/index/publicfiles/download/1/"+src+".png";
-						
+		
+		// create link 
 		$(this).find("img").wrap('<a class="gallery-link" href="'+src+'" title="" target="_gallery_slideshow"></a>');
+		
+		// change url of the image
 		$(this).find("img").attr("src", src);
 					
 	});
 	
-	
-	
+	// change the urls of normal images
 	$(".confluence-embedded-image").each(function(){
 		var src = $(this).attr("src").split("/").pop();
 		src = "http://localhost/ojs/index.php/index/publicfiles/download/1/"+src;
@@ -28,7 +31,6 @@ $(document).ready(function() {
 		$(this).find("a").attr("rel", "gallery-"+indexGallery);
 	});
 			
-
 	$('.gallery-link').fancybox();
 				
 });
