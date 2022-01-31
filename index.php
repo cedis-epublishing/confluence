31f3edf20143e2baadaf12f7b5afa7ae840a06ef
CFanselow <CFanselow@users.noreply.github.com>
2022-01-31 20:24:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
 /**
  * @file plugins/generic/confluence/index.php
  *
- * Copyright (c) 2013-2015 Simon Fraser University Library
- * Copyright (c) 2003-2015 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2022 Freie Universität Berlin
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_generic_confluence
  * @brief Wrapper for Confluence plugin.
